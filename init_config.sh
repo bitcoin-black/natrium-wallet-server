8a26c38b215baa37df5fd4a88dcd3103ceaa9a1f
tundak <sonu5650@gmail.com>
2020-03-16 16:58:56 +0530

    fix

--- init_config.sh
+++ init_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "RPC_URL=http://[::1]:37000" > .env
+echo "RPC_URL=http://134.209.31.161:35000" > .env
 echo "FCM_API_KEY=None" >> .env
-echo "FCM_SENDER_ID" >> .env
+echo "FCM_SENDER_ID=None" >> .env
 echo "DEBUG=1" >> .env
